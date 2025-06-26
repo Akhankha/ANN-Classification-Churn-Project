31645f7e1c00a2f5f9307a85f1b2e32644734d1b
Akhankha <84345990+Akhankha@users.noreply.github.com>
2025-06-26 22:09:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,9 +8,6 @@ import pickle
 # Load the trained model
 model = tf.keras.models.load_model('model.h5')
 
-# Load the trained model
-model = tf.keras.models.load_model('model.h5')
-
 # Load the encoders and scaler
 with open('label_encoder_gender.pkl', 'rb') as file:
     label_encoder_gender = pickle.load(file)
@@ -19,13 +16,11 @@ with open('one_hot_encoder_geo.pkl', 'rb') as file:
     one_hot_encoder_geo = pickle.load(file)
 
 with open('scaler.pkl', 'rb') as file:
-    scaler = pickle.load(file)  
+    scaler = pickle.load(file)
 
 
-# Streamlit app
-
-st.title('Customer Churn Prediction')   
-
+## streamlit app
+st.title('Customer Churn PRediction')
 
 # User input
 geography = st.selectbox('Geography', one_hot_encoder_geo.categories_[0])
